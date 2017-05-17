c747ffc98326d0bbc3a418e27d154d37c38e6ad1
Laurent Goderre <laurent.goderre@gmail.com>
2017-05-17 14:14:20 -0400

    Fixed the build by updating the java version

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 cp Dockerfile.template Dockerfile
 
 echo "\n#\n# Java\n#\n" >> Dockerfile
-curl -sSL --compressed  https://raw.githubusercontent.com/docker-library/openjdk/445f8b8d18d7c61e2ae7fda76d8883b5d51ae0a5/8-jre/Dockerfile | tail -n +8  >> Dockerfile
+curl -sSL --compressed  https://raw.githubusercontent.com/docker-library/openjdk/415b0cc42d91ef5d70597d8a24d942967728242b/8-jre/Dockerfile | tail -n +8  >> Dockerfile
 
 echo "\n#\n# Ruby\n#\n" >> Dockerfile
 curl -sSL --compressed  https://raw.githubusercontent.com/docker-library/ruby/bfc7a48724ceb1917ddbcb713b24c835eca584c8/2.4/Dockerfile | tail -n +3 | head -n -1  >> Dockerfile
