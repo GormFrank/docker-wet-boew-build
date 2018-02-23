1cf90ffa3e00650019e9b2973e26370491cacc91
Laurent Goderre <laurent.goderre@gmail.com>
2018-02-23 00:08:54 -0500

    Upgrade image to Node 8

--- update.sh
+++ update.sh
@@ -5,6 +5,6 @@ echo "\n#\n# Java\n#\n" >> Dockerfile
 curl -sSL --compressed  https://raw.githubusercontent.com/docker-library/openjdk/415b0cc42d91ef5d70597d8a24d942967728242b/8-jre/Dockerfile | tail -n +8  >> Dockerfile
 
 echo "\n#\n# Ruby\n#\n" >> Dockerfile
-curl -sSL --compressed  https://raw.githubusercontent.com/docker-library/ruby/bfc7a48724ceb1917ddbcb713b24c835eca584c8/2.4/Dockerfile | tail -n +3 | head -n -1  >> Dockerfile
+curl -sSL --compressed  https://raw.githubusercontent.com/docker-library/ruby/bfc7a48724ceb1917ddbcb713b24c835eca584c8/2.4/Dockerfile | tail -n +3 | tail -r | tail -n +2 | tail -r >> Dockerfile
 
 echo "RUN gem install rake html-proofer" >> Dockerfile
